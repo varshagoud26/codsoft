74827c17c98f7f416ba98997fdceb29467af9356
varshagoud26 <164031911+varshagoud26@users.noreply.github.com>
2024-05-08 18:54:00 +0530

    Create GradeCalculator.java

---
+++ GradeCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        char grade = calculateGrade(averagePercentage);
+
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    public static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else if (averagePercentage >= 50) {
+            return 'E';
+        } else {
+            return 'F';
+        }
+    }
+}
+
